6a51838db1f52c4cb9347ef289140e5e74ebe8af
Mohak Talreja <mohaktalreja0011@gmail.com>
2022-10-24 15:55:15 +0530

    Update NonAbstractDatatype.java

--- NonAbstractDatatype.java
+++ NonAbstractDatatype.java
@@ -12,7 +12,7 @@ public class NonAbstractDatatype {
       p1.x = 10;
       p1.y =20;
       Point p2 = p1;
-      p2.x = 30;
+      p2.x = 50;
       System.out.println(p1.x);
       System.out.println(p2.x);
 	}
